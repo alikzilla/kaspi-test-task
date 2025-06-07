37872f0fc495eb8cdffb05d5e08f99daa956f869
alikzilla <nuspekovalihan@gmail.com>
2025-06-07 22:43:20 +0500

    fix: correct import path for starHalf icon in index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,7 +2,7 @@ import percent from "./percent.svg";
 import search from "./search.svg";
 import settings from "./settings.svg";
 import shevronDown from "./shevron-down.svg";
-import starHalf from "./percent.svg";
+import starHalf from "./star-half.svg";
 import unavailable from "./unavailable.svg";
 import user from "./user.svg";
 
